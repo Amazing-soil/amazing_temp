6671ebebbf25f56ce17637691678544fb8e73418
Amazing-soil <454732172@qq.com>
2018-01-10 18:05:30 +0800

    取消总流量统计

--- 61run2.py
+++ 61run2.py
@@ -4,7 +4,27 @@
 # @Author  : yao.liu
 # @File    : 61run2.py
 
-import requests
-import json
+a = range(1,9)
+j = 0
+k = 0
+c = len(a)
+def amazing(a,j,k,c):
+    if j < c:
+        for i in a:
+            print a
+            print i,
+            a.remove(i)
+            j += 1
+        return amazing(a,j,k,c)
+    else:
+        if k < c:
+            k += 1
+            b = a[0]
+            a.remove(b)
+            a.append(b)
+            return amazing(a,j,k,c)
+        else:
+            print 'end'
 
-def
+amazing(a,j,k,c)
+
