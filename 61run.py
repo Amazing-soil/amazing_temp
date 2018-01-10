197c7beb37e02bd951bdf972ef25fe22960e8045
Amazing-soil <454732172@qq.com>
2018-01-10 18:06:15 +0800

    习题

--- 61run.py
+++ 61run.py
@@ -15,65 +15,136 @@ import getopt
 import signal
 reload(sys)
 sys.setdefaultencoding('utf8')
+helptext = u'''
+bre 设备对应频道对应时间，回源原因分析
+支持参数：
+-h \t输出帮助信息
+-t\t(必填)筛选时间区间,格式xxx(起始时间)-xxx（结束时间）,如筛选2017年5月10日19点的日志：-t 2017051019-2017051020
+--url\t(必填)筛选要过滤的频道或url，支持泛匹
+程序支持:bre自动化运维组<bre-ops-dev@chinacache.com>
+'''
 
-def check_squid_conf():
-    '''检查全局被动压缩配置，ok即存在'''
-    gzip_mod_check = commands.getoutput('grep -q mod_header_combination /usr/local/squid/etc/squid.conf && echo ok')
-    '''检查全局0方向是否配置删除特殊头，4即配置完整'''
-    del_0_header_check = commands.getoutput('egrep -c "(mod_header 0 del).*(allow all)" /usr/local/squid/etc/squid.conf')
-    '''检查所需日志特殊字段是否配置，ok即配置'''
-    squid_log_check = commands.getoutput('egrep "(logformat squid_custom_log)" /usr/local/squid/etc/squid.conf|grep -q -e "$AE" -e "$PB" && echo ok')
-    if gzip_mod_check == 'ok':
-        print u'全局配置被动压缩，压缩非压缩存两份，注意同文件会miss两次'.encode('utf8')
-    else:
-        print u'全局未配置被动压缩，压缩非压缩存一份，可能存在缓存问题，请确认功能需求'.encode('utf8')
-    if del_0_header_check == '4':
-        print u'设备已全局配置删除0方向影响缓存的特殊头'.encode('utf8')
-    else:
-        print u'设备未配置删除0方向影响缓存的特殊头，建议尝试配置删除0方向的Cache-Control If-None-Match If-Match If-Unmodified-Since'.encode('utf8')
-    if squid_log_check == 'ok':
-        return 1
-    else:
-        return 0
+def FormatTimerange(timerange):
+    '''处理时间戳'''
+    if '-' not in timerange:
+        print u'时间区间格式输入有误,正确示例 : 2017050901-2017050903'.encode('utf8')
+        exit()
+    try:
+        starttime = timerange.split('-')[0]
+        endtime = timerange.split('-')[1]
+    except:
+        print u'时间区间格式输入有误,正确示例 : 2017050901-2017050903'.encode('utf8')
+        exit()
+    try:
+        starttime = str(int(starttime)) + '0' * (10 - len(starttime))
+        endtime = str(int(endtime)) + '0' * (10 - len(endtime))
+    except:
+        print u'时间区间格式输入有误 : 开始时间和结束时间必须都是小于等于10位的时间戳数字 ,正确示例 : 2017050901-2017050903'.encode('utf8')
+        exit()
+    if starttime > endtime:
+        print u'开始时间不能大于结束时间'.encode('utf8')
+        exit()
+    print '开始时间:{0}\t结束时间:{1}'.format(int(starttime), int(endtime))
+    return {'starttime': int(starttime), 'endtime': int(endtime)}
 
-LineItem = {}
-special_log = {}
-num_url = {}
-with open('C:\\Users\\yao.liu\\Desktop\\test.log','r') as fn:
-    for i in fn.readlines():
-        LineItem['Url'] = i.split()[6].split('?')[0]
-        LineItem['Date'] = i.split()[0]
-        # 特殊日志字段收集，对应url
-        if not special_log.has_key(LineItem['Url']):
-            special_log[LineItem['Url']] = {'$AE':{'gzip': 0, 'ungzip': 0},'$PB':[],'Time':[]}
-        if LineItem['Date'] not in special_log[LineItem['Url']]['Time']:
-            special_log[LineItem['Url']]['Time'].append(LineItem['Date'])
-        for j in i.split('@in#(')[1].split('^~'):
-            if 'PB' in j:
-                if '-' not in j:
-                    hash_squid = j.split('.')[1]
-                    if hash_squid not in special_log[LineItem['Url']]['$PB']:
-                        special_log[LineItem['Url']]['$PB'].append(hash_squid)
-            if 'AE=' in j:
-                if j.split('=')[1] == 'gzip':
-                    special_log[LineItem['Url']]['$AE']['gzip'] += 1
-                else:
-                    special_log[LineItem['Url']]['$AE']['ungzip'] += 1
-        # url数量统计
-        if num_url.get(LineItem['Url']) is None:
-            num_url[LineItem['Url']] = 0
-        num_url[LineItem['Url']] += 1
+def GetFileName(starttime, endtime):
+    '''根据条件过滤文件名'''
+    filenamelist = []
+    listdir = os.listdir(logpath)
+    listdir.sort()
+    for filename in listdir:
+        try:
+            filetime = int(filename.split('.')[4][0:10])
+            filetype = filename.split('.')[-1]
+        except:
+            continue
+        if filetime >= int(starttime) and filetime <= int(endtime) and filetype == 'gz':
+            filenamelist.append(filename)
+    return filenamelist
 
-#url数量 top 5
-print u'请求url次数 TOP 5 :'.encode('utf8')
-top_num_url = sorted(num_url.iteritems(), key=lambda d: d[1], reverse=True)[:5]
-#for item in top_num_url:
-    # print special_log[item[0]]['$AE']['gzip'],special_log[item[0]]['$AE']['ungzip']
-    # print len(special_log[item[0]]['$PB'])
-print u'\t{0}\t{1}\t{2:>3}\t{3:>5}\t{4}'.format('次数','压缩\非压缩','hash个数','时间戳个数','URL').encode('utf8')
-for item in top_num_url:
-    print u'\t{0:<10}\t{1}\{2:<5}\t{3:<8}\t{4:<9}\t{5}'.format(item[1],
-                                                       special_log[item[0]]['$AE']['gzip'],
-                                                       special_log[item[0]]['$AE']['ungzip'],
-                                                       len(special_log[item[0]]['$PB']),
-                                                       len(special_log[item[0]]['Time']),item[0])
+def witchapp():
+    '''判断设备是FC，ats，hpcc'''
+    res = commands.getoutput('ccamr list | grep \' HPCC\'')
+    if 'HPCC' in res:
+        hpc_res = commands.getoutput(' rpm -q HPC ' )
+        if 'ATS' in hpc_res:
+            return 'ATS'
+        else:
+            return 'Hpcc'
+    else:
+        return 'Fc'
+
+def LogTogether(filenamelist):
+    '''将筛选访问日志聚合'''
+    #print u'\n正在打开日志包...'.encode('utf8')
+    for filename in filenamelist:
+        #print filename
+        loggzpath = '{0}{1}'.format(logpath, filename)
+        greplogpath = '/tmp/grep_61_log'
+        os.system('''zcat {0} | egrep '{1}' >> {2}'''.format(loggzpath, url, greplogpath))
+    return greplogpath
+
+def opts_get():
+    '''收集脚本,返回opts_dir字典'''
+    opts_dir = {}
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ht:", ["url=","cip=","ip1=","ip2="])
+    except Exception, e:
+        print e
+        exit()
+    for op, value in opts:
+        if op == '-h' or op == '--help':
+            print '{0}'.format(helptext.encode('utf8'))
+            exit()
+        elif op == '-t':
+            opts_dir['timerange'] = value
+        elif op == '--url':
+            opts_dir['url'] = value
+        elif op == '--cip':
+            opts_dir['cip'] = value
+        elif op == '--ip1':
+            opts_dir['ip1'] = value
+        elif op == '--ip2':
+            opts_dir['ip2'] = value
+    return opts_dir
+
+
+if __name__ == '__main__':
+    # 收集脚本参数
+    opts_dir = opts_get()
+    #tc = toolscount(username=opts_dir.get('username'), argv=str(opts_dir))
+    # 必要参数补充默认值
+    timerange = opts_dir.get('timerange')
+    if timerange is None and opts_dir.get('log') is None:
+        print u'-t 和 --url= 参数为必填项,-t格式xxx(起始时间)-xxx（结束时间）,--url=http://chinacache.com  如筛选2017年5月10日19点的日志:-t 2017051019-2017051020,使用-h可以查看帮助'.encode('utf8')
+        exit()
+    url = opts_dir['url']
+    app = witchapp()
+    logpath = {"Fc": "/data/proclog/log/squid/access/", "Hpcc": "/data/proclog/log/hpc/access/",
+               "ATS": "/data/proclog/log/hpc/access/"}.get(app)
+    Time_use = FormatTimerange(opts_dir['timerange'])
+    greplogpath = LogTogether(GetFileName(Time_use['starttime'], Time_use['endtime']))
+    if not os.path.exists(greplogpath):
+        print u'选中的时间段内没有找到对应日志包,请确认时间段是否有误'.encode('utf8')
+        exit()
+    with open(greplogpath, 'r') as f1:
+        # 得到日志各字段
+        LineItem = {}
+        Compare_url = {}
+        # 逐行数据记录
+        sumnum_mate = 0
+        for sumnum, line in enumerate(f1):
+            try:
+                LineItem['ClientIp'] = line.split()[2]
+                LineItem['Url'] = line.split()[6]
+            except:
+                break
+        if LineItem['ClientIp'] == opts_dir['cip']:
+            sumnum_mate += 1
+            if not Compare_url.has_key(LineItem['Url']):
+                Compare_url[LineItem['Url']] = {opts_dir['ip1']:{'gzip':'','unzip':''},opts_dir['ip2']:{'gzip':'','unzip':''}}
+        if sumnum_mate == 0:
+            print '没有筛选到符合条件的日志'
+            os.remove(greplogpath)
+            exit()
+    print Compare_url
