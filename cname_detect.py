11cdf2c5a00e4da8a1b35ae9629e68dc980a990a
Amazing-soil <454732172@qq.com>
2018-02-27 17:23:58 +0800

    切回域名检测临时需求

---
+++ cname_detect.py
@@ -0,0 +1,43 @@
+#!/opt/hopeservice/.venv/bin/python
+# -*- coding: UTF-8 -*-
+# @Time    : 27/02/2018 10:00
+# @Author  : yao.liu
+# @File    : cname_detect.py
+# version  : 1.0
+
+import commands
+import sys
+import requests
+from datetime import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def cname_check(domain):
+    '''dig测试域名'''
+    cmd = ' dig @8.8.8.8 {0} +short|grep -E "ccgslb|chinacache" |head -1 '.format(domain)
+    res = commands.getoutput(cmd)
+    if res:
+        return res
+    else:
+        return None
+
+def sendmail(content):
+    '''发送告警邮件'''
+    #填写收件人
+    data = {'tos': 'yao.liu@chinacache.com',
+            'subject': '[devops] cname_detect -域名已切回蓝汛加速',
+            'content': '{}'.format(content)}
+    res = requests.post('http://223.202.201.32:8700/email', data=data)
+
+if __name__ == '__main__':
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    domain_file = sys.argv[1]
+    with open(domain_file,'r') as fn:
+        for i in fn.readlines():
+            if cname_check(i):
+                content = '''<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><b>您好【{0}】:</b><table border="1"> \
+                <tbody><tr> <td><div style="text-align:center;margin:0;" align="center"><b>域名切回至蓝汛服务</b></td> </tr><td>{1}'''.format(time_now,i)
+                sendmail(content)
+
+
